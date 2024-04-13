5246f44f255573e63bccf4fad5d9d96778a65d04
najiibmohamed11 <xaawomaxamuudkaahiye@gmail.com>
2024-04-13 23:52:28 +0300

    aded hive database

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,23 @@
 import 'package:debt_manager/pages/home.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding
+      .ensureInitialized(); // Ensure initializations occur before runApp
+
+  try {
+    await Hive.initFlutter(); // Initialize Hive for the Flutter environment
+
+    var debtorsBox = await Hive.openBox('debtorsBox'); // Open a box for debtors
+    var itemsBox = await Hive.openBox('itemsBox'); // Open a box for items
+
+    runApp(const MyApp()); // Run the app
+  } catch (e) {
+    print(
+        'Error initializing app: $e'); // Print any errors during initialization
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -10,6 +25,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: const Home());
+    return MaterialApp(
+      home: const Home(),
+      theme: ThemeData(
+        // Optional: Define a theme for the application
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      debugShowCheckedModeBanner: false, // Disable the debug banner
+    );
   }
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,8 +1,10 @@
 import 'dart:ffi';
+import 'dart:math';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:hive/hive.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -11,10 +13,107 @@ class Home extends StatefulWidget {
   State<Home> createState() => _HomeState();
 }
 
+String? nameofthedebter;
+
+final debtorsBox = Hive.box("debtorsBox");
+void addNewDebtor(String newDebtor) {
+  // Get the current highest key in the box
+  int newKey = 1;
+  if (debtorsBox.isNotEmpty) {
+    final lastKey = debtorsBox.keys.cast<int>().reduce(max);
+    newKey = lastKey + 1;
+  }
+
+  // Put the new debtor in the box with an auto-incremented key
+  debtorsBox.put(newKey, newDebtor);
+}
+
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
+    String? selectedCategory = "famale";
+    final List<String> categories = ['male', 'famale']; // Examp
+    Future<void> opendilogbox() => showDialog(
+          context: context,
+          builder: (context) => Dialog(
+            child: Container(
+              width:
+                  MediaQuery.of(context).size.width * 0.9, // Adjust width here
+              constraints: BoxConstraints(
+                minHeight: 200.0, // Minimum height
+                maxHeight:
+                    MediaQuery.of(context).size.height * 0.5, // Maximum height
+              ),
+              child: AlertDialog(
+                title: Text("Add New Debt"),
+                content: Column(
+                  mainAxisSize:
+                      MainAxisSize.min, // Adjust column height to fit contents
+                  children: [
+                    TextField(
+                      onChanged: (value) {
+                        setState(() {
+                          nameofthedebter = value;
+                        });
+                      },
+                      decoration: InputDecoration(
+                        hintText: "Name",
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: InputBorder.none,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    DropdownButtonFormField<String>(
+                      value: selectedCategory,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                      ),
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          selectedCategory = newValue;
+                        });
+                      },
+                      items: categories
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                    ),
+                  ],
+                ),
+                actions: [
+                  MaterialButton(
+                    onPressed: () {
+                      setState(() {
+                        addNewDebtor(nameofthedebter!);
+                      });
+                      Navigator.pop(context);
+                    },
+                    child: Text(
+                      "Add",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    color: Colors.green,
+                  )
+                ],
+              ),
+            ),
+          ),
+        );
+
     return Scaffold(
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        opendilogbox();
+      }),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
@@ -95,7 +194,27 @@ class _HomeState extends State<Home> {
               SizedBox(
                 height: 25.0,
               ),
-              DebtorsCard(),
+              // debtorsBox.isEmpty
+              //     ? Text("no one is in you'r debtors")
+              //     :
+              Expanded(
+                child: ListView.separated(
+                  itemCount: debtorsBox.length,
+                  itemBuilder: (context, index) {
+                    return DebtorsCard(
+                      Total_dollor: 12.0,
+                      Total_shiling_somali: 20,
+                      imagepath: selectedCategory == 'famale'
+                          ? "images/hijabgirl.png"
+                          : "images/black man.png",
+                      titile: debtorsBox.getAt(index),
+                    );
+                  },
+                  separatorBuilder: (context, index) =>
+                      Divider(), // Adds a divider between each item
+                ),
+              ),
+
               Divider()
             ],
           ),
@@ -106,9 +225,18 @@ class _HomeState extends State<Home> {
 }
 
 class DebtorsCard extends StatelessWidget {
-  const DebtorsCard({
-    super.key,
+  DebtorsCard({
+    required this.imagepath,
+    required this.titile,
+    // required this.mounth,
+    required this.Total_dollor,
+    required this.Total_shiling_somali,
   });
+  String? imagepath;
+  String? titile;
+  // String? mounth;
+  double? Total_dollor;
+  int? Total_shiling_somali;
 
   @override
   Widget build(BuildContext context) {
@@ -119,22 +247,21 @@ class DebtorsCard extends StatelessWidget {
         decoration: BoxDecoration(
             color: Color(0xffF1EBE2),
             borderRadius: BorderRadius.circular(12.0)),
-        child: Image.asset("images/hijabgirl.png"),
+        child: Image.asset(imagepath!),
       ),
       title: Text(
-        "Amina",
+        titile!,
         style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
       ),
-      subtitle: Text("april"),
+      // subtitle: Text(mounth!),
       trailing: Column(
         children: [
           Text(
-            "\$12.0",
-            style: TextStyle(
-                fontSize: 18.0, fontWeight: FontWeight.bold),
+            "\$${Total_dollor}",
+            style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
           ),
           Text(
-            "20k",
+            "$Total_shiling_somali k",
             style: TextStyle(
               fontSize: 15.0,
             ),
